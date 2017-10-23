53d64a673cce42aeeca7a0e56e5381d4a0594deb
szato <tszanyi@brainsum.com>
2017-10-23 16:53:31 +0200

    Auto trigger on select.

--- js/modal.js
+++ js/modal.js
@@ -3,11 +3,15 @@
   'use strict';
   Drupal.behaviors.mediaBox = {
     attach: function (context, settings) {
+      var $insert_button = $("#filefield_filesources_jsonapi_action input[name='insert_selected']");
+      $insert_button.hide();
+
       $('.form-type-checkbox + label').each(function(){
         $(this).find('img').wrapAll('<div class="form-image" />').wrapAll('<div class="image" />');
       });
       $('input.form-checkbox').on('click', function() {
         var $parent = $(this).closest('.form-type-checkbox');
+        $insert_button.mousedown();
         if ($(this).is(':checked')) {
           $parent.addClass('checked');
         }
